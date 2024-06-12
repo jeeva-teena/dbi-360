b013a47bf87000048cbdf2b833f795a88f520705
TeenaGupta <teena.g@greenjeeva.com>
2024-06-13 00:07:41 +0530

    Revert "changes"
    This reverts commit fc93f3d350f65709b2b8ae03d0711ae0bc7a8491.

--- next.config.js
+++ next.config.js
@@ -1,30 +1,13 @@
 /**
 * @type {import('next').NextConfig}
 */
-module.exports = {
+const nextConfig = {
   output: "export",
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    loader: "akamai",
+    path: "",
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/home",
-  //     },
-  //     {
-  //       source: "/admin",
-  //       destination: "/admin/index.html",
-  //     },
-  //   ];
-  // },
+  assetPrefix: "./",
 };
 
+export default nextConfig;
