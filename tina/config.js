d3fa988c5208f4e50ac1abd51e605972bc731b6f
TeenaGupta <teena.g@greenjeeva.com>
2024-06-12 11:15:38 +0530

    changes

--- tina/config.js
+++ tina/config.js
@@ -1,5 +1,12 @@
+require("ts-node").register();
+module.exports = require("./config.tsx");
 import { LocalAuthProvider } from "tinacms";
 import { UsernamePasswordAuthJSProvider, TinaUserCollection } from "tinacms-authjs/dist/tinacms";
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
-require("ts-node").register();
-module.exports = require("./config.tsx");
+const authProvider = isLocal
+? LocalAuthProvider()
+: UsernamePasswordAuthJSProvider({
+    database: new TinaUserCollection(),
+    secret: process.env.NEXTAUTH_SECRET,
+  })
+  
