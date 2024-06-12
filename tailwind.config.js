7e99ef2635136e9f2c54f4c11ef790ecd295a396
TeenaGupta <teena.g@greenjeeva.com>
2024-06-13 00:07:49 +0530

    Revert "update"
    This reverts commit 66474e6c7b1c4eb5e7c15dec143ef12e7c23627c.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     colors: {
