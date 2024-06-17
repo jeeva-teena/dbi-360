dfca9ef03c1180ea2ee5f9c4b6ed66319a649f8f
TeenaGupta <teena.g@greenjeeva.com>
2024-06-17 16:58:38 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -8,16 +8,16 @@ module.exports = {
 
     return config;
   },
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/home",
-      },
-      {
-        source: "/admin",
-        destination: "/admin/index.html",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/home",
+  //     },
+  //     {
+  //       source: "/admin",
+  //       destination: "/admin/index.html",
+  //     },
+  //   ];
+  // },
 };
