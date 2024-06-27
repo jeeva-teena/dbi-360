0132c983a48c14391938c70f0362cffe85e331ee
TeenaGupta <teena.g@greenjeeva.com>
2024-06-27 09:31:15 +0530

    changes

--- tina/config.tsx
+++ tina/config.tsx
@@ -6,25 +6,20 @@ import Author from "./collection/author";
 import Page from "./collection/page";
 
 const config = defineConfig({
-  contentApiUrlOverride: '/api/tina/gql',
+  contentApiUrlOverride: "/api/tina/gql",
   branch: process.env.VERCEL_GIT_COMMIT_REF || "main",
-  clientId: process.env.TINA_CLIENT_ID || "c65f42f8-3b2b-4cc2-b8ec-3272951b2d98",
+  clientId:
+    process.env.TINA_CLIENT_ID || "c65f42f8-3b2b-4cc2-b8ec-3272951b2d98",
   token: process.env.TINA_TOKEN || "7445b0f6803e320aa0a6b8058f97fea6b73be2a3",
   media: {
-    // If you wanted cloudinary do this
-    // loadCustomStore: async () => {
-    //   const pack = await import("next-tinacms-cloudinary");
-    //   return pack.TinaCloudCloudinaryMediaStore;
-    // },
-    // this is the config for the tina cloud media store
     tina: {
       publicFolder: "public",
       mediaRoot: "uploads",
     },
   },
   build: {
-    publicFolder: "public", // The public asset folder for your framework
-    outputFolder: "admin", // within the public folder
+    publicFolder: "public",
+    outputFolder: "admin",
   },
   schema: {
     collections: [Post, Global, Author, Page],
