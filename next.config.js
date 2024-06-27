37eb5260319dfcc391600d83e1458012c623d8f1
TeenaGupta <teena.g@greenjeeva.com>
2024-06-27 09:41:29 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -3,15 +3,14 @@ const { hostname } = require("os");
 module.exports = {
   reactStrictMode: true,
   env: {
+    contentApiUrlOverride: "/api/tina/gql",
+    branch: process.env.VERCEL_GIT_COMMIT_REF || "",
+    clientId: process.env.TINA_CLIENT_ID || "",
+    token: process.env.TINA_TOKEN || "",
     KV_REST_API_URL: "https://unified-possum-54857.upstash.io",
     KV_REST_API_TOKEN:
       "AdZJAAIncDE0NTk3NzJiNTJiZmY0MzEwYjc4M2E2ZWEyOTUwOWRlZHAxNTQ4NTc",
-    NEXTAUTH_SECRET: "4f52dccf94c0dc1d1d42515477790c34",
     TINA_PUBLIC_IS_LOCAL: false,
-    TINA_CLIENT_ID: "c65f42f8-3b2b-4cc2-b8ec-3272951b2d98",
-    TINA_TOKEN: "7445b0f6803e320aa0a6b8058f97fea6b73be2a3",
-    contentApiUrlOverride: "/api/tina/gql",
-    branch: process.env.VERCEL_GIT_COMMIT_REF || "main",
   },
   webpack(config) {
     config.module.rules.push({
