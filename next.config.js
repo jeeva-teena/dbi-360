b4615a3dba230269d17f80f953d0acbb8c4dc468
TeenaGupta <teena.g@greenjeeva.com>
2024-06-27 09:44:51 +0530

    changes update

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@ const { hostname } = require("os");
 
 module.exports = {
   reactStrictMode: true,
-  env: {
-    contentApiUrlOverride: "/api/tina/gql",
-    branch: process.env.VERCEL_GIT_COMMIT_REF || "",
-    clientId: process.env.TINA_CLIENT_ID || "",
-    token: process.env.TINA_TOKEN || "",
-    KV_REST_API_URL : process.env.KV_REST_API_URL || "",
-    TINA_PUBLIC_IS_LOCAL: false,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
