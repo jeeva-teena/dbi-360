5fec24423604b900a3472316bf621eaffa278ea7
TeenaGupta <teena.g@greenjeeva.com>
2024-07-02 12:15:48 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 module.exports = {
+  reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
